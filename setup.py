df1c8a46163d7bfdf79f17caa8cb806957e48b0a
gnuxie <gnuxie@element.io>
2022-02-15 09:38:09 +0000

    v0.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="limiter",
-    version="0.0.1",
+    version="0.0.4",
     packages=find_packages(),
     description="Limiter",
     include_package_data=True,
